cd5cb53ecbe39f1a1aafe5382dcf1a6c5a6db468
unknown <1034832478@qq.com>
2019-04-04 21:10:24 +0800

    upload assets

---
+++ Assets/scripts/CubeScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    public Rigidbody cubeRigidBody;
+    public float torqueUnit; // torque unit
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // ector3(0, 1, 0)
+        this.cubeRigidBody.AddTorque(10f,10f,10f, ForceMode.Force );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // float turnY = Input.GetAxis("Vertical");
+        // float turnX = Input.GetAxis("Horizontal");
+        // this.cubeRigidBody.AddTorque(  turnY * torqueUnit , 0, -turnX * torqueUnit);
+    }
+}
---
+++ Assets/scripts/MoveBallScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBallScript : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        // 根据转矩来移动小球
+        float turnY = Input.GetAxis( "Vertical");
+        float turnX = Input.GetAxis("Horizontal");
+        this.rigidbody.AddTorque(turnY * speed ,0 , -turnX * speed );
+    }
+}
